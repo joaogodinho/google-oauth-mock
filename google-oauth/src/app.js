371f38f387ccf50261f73ca2ba1e312fd756477b
João Godinho <joaocfgodinho@gmail.com>
2019-05-02 10:23:21 +0100

    Feat: Make use of env, reduce image sizes

--- google-oauth/src/app.js
+++ google-oauth/src/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const DOMAIN = process.env.DOMAIN;
+var CLIENT_ID;
+
 app.get('/', (req, res) => res.send('Hello I\'m a fake google!'))
 
 // Redirect user to received URI with state
@@ -17,6 +20,7 @@ app.get('/o/oauth2/auth', (req, res) => {
       scope = req.query.scope;
   let redirectFinal = `${redirectUri}?state=${state}&scope=${scope}&code=${code}`
 
+  CLIENT_ID = req.query.client_id;
   debug(`Redirecting to: ${redirectFinal}`);
   res.redirect(redirectFinal);
 });
@@ -27,11 +31,11 @@ app.post('/token', (req, res) => {
 
   let id_token = {
     'iss': 'https://accounts.google.com',
-    'azp': '*.apps.googleusercontent.com',
-    'aud': '*.apps.googleusercontent.com',
+    'azp': `${CLIENT_ID}`,
+    'aud': `${CLIENT_ID}`,
     'sub': '0',
-    'hd': '<domain>',
-    'email': 'john.doe@<domain>',
+    'hd': `${DOMAIN}`,
+    'email': `john.doe@${DOMAIN}`,
     'email_verified': true,
     'at_hash': '',
     'name': 'John Doe',
@@ -43,12 +47,13 @@ app.post('/token', (req, res) => {
   }
 
   let fakeToken = {
-    'access_token': 'ya29.GlzzBhK-d0aPpC2wTzE5WtdudVRyieYKU3Z1cjWn9yPHJsbksdJ0lUgu8MHBNQnIdSLG9hIMoi3dHLosj2Cj3cfcHwShloScoQQ_8omOREB86ZSyt9EfysA87rHoYQ',
+    'access_token': 'fake_access_token',
     'expires_in': 3600,
     'scope': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
     'token_type': 'Bearer',
     'id_token': `eyJhbGciOiJSUzI1NiIsImtpZCI6IjI2ZmM0Y2QyM2QzODdjYmM0OTBmNjBkYjU0YTk0YTZkZDE2NTM5OTgiLCJ0eXAiOiJKV1QifQ.${Buffer.from(JSON.stringify(id_token)).toString('base64')}.MmesRidSSQrnPT2kk2iOuMaZO8KOM0dbykHdfPbk5MmZ48hJwNPjOWtRkgEm2Opv28B1m4qwRAoZ3TIa08oICqswpRIKuHk_CYglBsdj5d490k8QpLXM87z2cm6E91G3aJ-KnbJoPdsxYV_WhTSDOUIt3kW7EYOIWqrNvKJtMD65SrKQeifCOf9lJtZz1Ku61L_Eo0te1Kv03wrdclLTP6LmAxUYNsN3HUKZIaVSF0SEJKPLSZU4Sq4pOWoSgHU0qYXDfCPsHkp_Z8syoLZniNsmg8CuXaNVMDIaUALw5KRN2XxEk6vRJMjbVGuBi2yCjhGZDyCfoIskfECQP2Pvdg`
   }
+
   debug('Sending:');
   debug(fakeToken);
   res.send(fakeToken);
@@ -64,8 +69,8 @@ app.get('/oauth2/v3/userinfo', (req, res) => {
     'locale': 'en', 
     'email_verified': true, 
     'given_name': 'John', 
-    'email': 'john.doe@<domain>', 
-    'hd': '<domain>',
+    'email': `john.doe@${DOMAIN}`, 
+    'hd': `${DOMAIN}`,
     'name': 'John Doe'
   }
 
--- inject_mock.sh
+++ inject_mock.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -x
+set -xe
 
-readonly NETWORK="<target network>"
-readonly NGINX_CONTAINER="dynamic-framework_nginx_1"
-readonly TARGET_CONTAINER="<target container>"
-readonly CERT="rootCA.pem"
+source .env
+readonly CERT="$(mkcert -CAROOT)/rootCA.pem"
 readonly IP=$(docker inspect --format='{{ (index .NetworkSettings.Networks "'"$NETWORK"'").IPAddress }}' $NGINX_CONTAINER)
 
 # Copy mkcert cert
@@ -12,4 +10,8 @@ docker cp "$CERT" "$TARGET_CONTAINER:/usr/local/share/ca-certificates/rootCA.pem
 # Add certificate to chain
 docker exec $TARGET_CONTAINER update-ca-certificates
 # Inject mock IP in hosts
-docker exec $TARGET_CONTAINER bash -c "echo -e \"$IP\toauth2.googleapis.com\n$IP\twww.googleapis.com\n$IP\taccounts.google.com\" >> /etc/hosts"
+docker exec $TARGET_CONTAINER bash -c "cat >> /etc/hosts << EOF
+$IP    oauth2.googleapis.com
+$IP    www.googleapis.com
+$IP    accounts.googleapis.com
+EOF"
